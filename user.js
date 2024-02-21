21c4d997e151fdff806907f51a140473bab86381
yazeedcrash <140242342+yazeedcrash@users.noreply.github.com>
2024-02-21 13:40:22 +0300

    v2

--- user.js
+++ user.js
@@ -1,30 +1,74 @@
+const { urlencoded } = require('body-parser')
 const express = require('express')
 var app = express()
 var fs= require('fs')
+
+var bodyParser= require('body-parser')
+var urlEncoded= bodyParser.urlencoded({extended:false})
+
+
 app.get('/',function(req,res){
     res.send("start")
 })
 
+app.get('/form', function(req,res)
+{
+    res.sendFile(__dirname+'/form.html', )
+    
+})
+
+
 app.get('/listUsers',function(req,res){
-    res.send("list users")
-    var data= fs.readFileSync(__dirname+'users.json')
+    // res.send("list users")
+    var data= fs.readFileSync(__dirname+"/users.json")
+
     res.send(String(data))
 })
-app.get('/user/:id',function(req,res){
-    res.send("select user")
+app.get('/userById/:id',function(req,res){
+    // res.send("select user")
+    var arr =["1", "2", "3"]
+    if (arr.includes(String(req.params.id)))
+    {
+    var data= fs.readFileSync(__dirname+"/users.json") //as byte
+    data= JSON.parse(String(data)) //parse to change string to key value format
+    
+    var user = data['user'+req.params.id]
+   
+
+    res.send(user)
+    }
+    else
+    {
+      res.send("user id error")
+    }
 })
 
-app.get('/deleteUser',function(req,res){
-    res.send("user deleted")
+app.delete('/deleteUser/:id',function(req,res){
+    var data= fs.readFileSync(__dirname+"/users.json") 
+    data= JSON.parse(String(data))
+    
+
+    var user = data['user'+req.params.id]
+    delete data['user'+req.params.id]
+    res.send(data)
 })
 
-app.get('/addUser',function(req,res){
-    res.send("user added")
-})
 
 
 var server= app.listen(7005, function()
 {
     var host = server.address().address   
     var port = server.address().port 
+})
+
+app.post('/addUser', urlEncoded,function(req,res) ///postman 
+{
+    var addData={name:"", password:"", jobList:""}
+    addData.name=req.body.name
+    addData.password=req.body.password
+    addData.jobList= req.body.jobList
+    var data= fs.readFileSync(__dirname+"/users.json") 
+    data= JSON.parse(String(data))
+    data['user4']= addData
+    res.send(data)
 })
